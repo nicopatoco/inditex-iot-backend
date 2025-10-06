9e47be7c52c74d122c1754b85dc29bb3634453f3
nicoKloov <nicodev@kloov.com>
2025-10-06 16:03:08 +0200

    chore(db): seed H2 with example PRICES dataset and defer data initialization

---
+++ src/main/resources/data.sql
@@ -0,0 +1,15 @@
+INSERT INTO prices (brand_id, product_id, price_list, priority, price, curr, start_date, end_date)
+VALUES
+(1, 35455, 1, 0, 35.50, 'EUR', TIMESTAMP '2020-06-14 00:00:00', TIMESTAMP '2020-12-31 23:59:59');
+
+INSERT INTO prices (brand_id, product_id, price_list, priority, price, curr, start_date, end_date)
+VALUES
+(1, 35455, 2, 1, 25.45, 'EUR', TIMESTAMP '2020-06-14 15:00:00', TIMESTAMP '2020-06-14 18:30:00');
+
+INSERT INTO prices (brand_id, product_id, price_list, priority, price, curr, start_date, end_date)
+VALUES
+(1, 35455, 3, 1, 30.50, 'EUR', TIMESTAMP '2020-06-15 00:00:00', TIMESTAMP '2020-06-15 11:00:00');
+
+INSERT INTO prices (brand_id, product_id, price_list, priority, price, curr, start_date, end_date)
+VALUES
+(1, 35455, 4, 1, 38.95, 'EUR', TIMESTAMP '2020-06-15 16:00:00', TIMESTAMP '2020-12-31 23:59:59');
