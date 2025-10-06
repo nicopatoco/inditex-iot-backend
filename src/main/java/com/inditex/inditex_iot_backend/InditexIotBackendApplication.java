864eef6426612c604da579b96a74cfe32506e00b
nicoKloov <nicodev@kloov.com>
2025-10-06 12:21:37 +0200

    chore: bootstrap Spring Boot pricing service

---
+++ src/main/java/com/inditex/inditex_iot_backend/InditexIotBackendApplication.java
@@ -0,0 +1,13 @@
+package com.inditex.inditex_iot_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InditexIotBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InditexIotBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inditex/inditex_iot_backend/InditexIotBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inditex.inditex_iot_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InditexIotBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
